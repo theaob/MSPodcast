c6190105e6ff85f91a86f024e5da267f0e414522
Onur Baykal <theaob@gmail.com>
2012-12-04 22:11:03 +0200

    project init

---
+++ Modern Sabahlar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Modern Sabahlar
+//
+//  Created by Onur Baykal on 04.12.2012.
+//  Copyright (c) 2012 Onur Baykal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MSAppDelegate class]));
+    }
+}
